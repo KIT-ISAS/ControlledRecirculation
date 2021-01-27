cb549aacd398e728083f82dc648196ea85bf5439
JonathanVieth <74063460+JonathanVieth@users.noreply.github.com>
2021-01-27 16:15:58 +0100

    Add files via upload
    Mehrere einfache Regler zusätzlich implementiert. Teilweise sollten die Tozeiten und die Smith predictor nocheinmal untersucht werden. Außerdem sollte das ganze Modell besser dokumentiert werden und verschiedene Szenarien implementiert werden

--- Simulink/guetemass.m
+++ Simulink/guetemass.m
@@ -8,7 +8,7 @@ function [J] = guetemass(x,n_n,c,alpha,beta, opt)
 %       -r_n
 switch opt
     case 1
-        J = sum((x(2*n_n+1:3*n_n)- 5*ones(n_n,1)).*(x(2*n_n+1:3*n_n)- 5*ones(n_n,1)) + (x(3*n_n+1:4*n_n)- 2*ones(n_n,1)).*(x(3*n_n+1:4*n_n)- 2*ones(n_n,1)));
+        J = sum((x(2*n_n+1:3*n_n)- 5*ones(n_n,1)).^2 + (x(3*n_n+1:4*n_n)- 2*ones(n_n,1)).^2);
     case 2
         % Ausgaenge
         y = calcOutput(x,alpha,beta);
@@ -23,6 +23,7 @@ switch opt
         J = c(1)*sum(tp(:)) + c(2)*sum(fp(:))+c(3)*sum(fn(:)) + c(4)*sum(tn(:));
     otherwise
         J = 0;
+     
 end
 end
 
